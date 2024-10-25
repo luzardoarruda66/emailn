7f77a79e1adfd8247eed5a2372e89a3b6d15e04e
Luzardo Arruda <desenvolvedor.luzardo@gmail.com>
2024-10-25 18:44:35 -0300

    added unit test to handler error

---
+++ internal/endpoints/handler_error_test.go
@@ -0,0 +1,26 @@
+package endpoints
+
+import (
+	internalerrors "emailn/internal/internalErrors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
+	assert := assert.New(t)
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, internalerrors.ErrInternal
+	}
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusInternalServerError, res.Code)
+	assert.Contains(res.Body.String(), internalerrors.ErrInternal.Error())
+
+}
