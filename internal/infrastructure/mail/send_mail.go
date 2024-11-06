48a51f7e8e09838d1cf096f109c9984f77dd2b9c
Luzardo Arruda <desenvolvedor.luzardo@gmail.com>
2024-11-06 16:01:02 -0300

    created func to send_email

---
+++ internal/infrastructure/mail/send_mail.go
@@ -0,0 +1,27 @@
+package mail
+
+import (
+	"emailn/internal/domain/campaign"
+	"fmt"
+	"os"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendEmail(campaign *campaign.Campaign) error {
+	fmt.Println("Sending email..")
+
+	d := gomail.NewDialer(os.Getenv("EMAIL_SMTP"), 587, os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"))
+
+	var emails []string
+	for _, email := range campaign.Contacts {
+		emails = append(emails, email.Email)
+	}
+	m := gomail.NewMessage()
+	m.SetHeader("From", os.Getenv("EMAIL_USER"))
+	m.SetHeader("To", emails...)
+	m.SetHeader("Subject", campaign.Name)
+	m.SetBody("text/html", campaign.Content)
+
+	return d.DialAndSend(m)
+}
